1064caabf2aea026f312f65a919a830149477032
hyungilee <mike.hyungi.lee@gmail.com>
2020-04-18 14:13:49 +0900

    [update] Add bundle process to transfer the data between fragments.

---
+++ app/src/main/java/com/hyungilee/androidjetpacknavigationsample/MainFragment.kt
@@ -0,0 +1,46 @@
+package com.hyungilee.androidjetpacknavigationsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentController
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // reference of the view
+        navController = Navigation.findNavController(view)
+        view_transactions_btn.setOnClickListener(this)
+        send_money_btn.setOnClickListener(this)
+        view_balance_btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        // backStack관리가 자동으로 이루어지기 때문에 편리하다.
+        when(v!!.id){
+            R.id.view_transactions_btn -> navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.send_money_btn -> navController.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+            R.id.view_balance_btn -> navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
+    }
+
+}
